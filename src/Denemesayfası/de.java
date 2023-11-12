fe8df59c10bf92c67f5957be03fd0d5a54d380cc
burak <sancakburak20@gmail.com>
2023-11-12 21:49:53 +0300

    manav proje eklendi

--- src/Denemesayfası/de.java
+++ src/Denemesayfası/de.java
@@ -7,18 +7,9 @@ import java.util.List;
 
 public class de {
     public static void main(String[] args) {
-        int[] numbers = {5, 2, 9, 3, 6, 1};
-        System.out.println("Orjinal array ="+ Arrays.toString(numbers));
-        int temp;
 
-            for (int j = 0; j < numbers.length-1; j++) {
-                if (numbers[j]>numbers[j+1]){
-                    temp=numbers[j];
-                    numbers[j]=numbers[j+1];
-                    numbers[j+1]=temp;
-                }
-            }
-
-        System.out.println("Sirali array ="+Arrays.toString(numbers));
+        int d =-35;
+        int g=-8;
+        System.out.println( d %g);
     }
 }
