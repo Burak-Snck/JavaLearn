578c4b94ee09966bbf9d7c27acf9ecaf810df550
burak <sancakburak20@gmail.com>
2023-12-15 23:01:01 +0300

    updated

---
+++ src/j29_Collection/p03_Queue/Queue.java
@@ -0,0 +1,25 @@
+package j29_Collection.p03_Queue;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.TreeMap;
+
+public class Queue {
+
+
+    public static void main(String[] args) {
+
+
+
+         /*
+Queue coll. interface oldg için child calss olan LinkedList ve PriorityQueue
+ile obj uretilir.
+Eczane yemekhane vs belirli şart ile aksiyon alan elemanlar için kullanılır.
+Const seçimine göre queue obj özelliklieri belirlenir.
+FİFO -> first in first out: Coll. ilk giren eleman ilk çıkar.Elemen en sona eklenir enbaştan silinir.
+ */
+
+        java.util.Queue<String> q1=new LinkedList<>(Arrays.asList("javaCAN","javVİDAN","jaVİYE","javaSU","javaHAN","javaZADE"));
+        TreeMap<Integer,String>
+    }
+}
