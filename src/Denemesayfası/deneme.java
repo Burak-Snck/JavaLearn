9d71e4ad8123fdcbf9e5a4667f813d6832b43225
burak <sancakburak20@gmail.com>
2023-12-31 01:22:37 +0300

    updated

--- src/Denemesayfası/deneme.java
+++ src/Denemesayfası/deneme.java
@@ -7,29 +7,15 @@ import java.util.SortedMap;
 public class deneme {
 
 
+    /* Write a return method  that can find the unique characters from the String
+     Ex:unique("AAAABBBCCCDEF") => "DEF"
+             */
     public static void main(String[] args) {
-        //Task-> int arr[][]={
-        //                {3,5},//0. kat
-        //                {12,13,15},//1. kat
-        //                {103,107,111,121},//2. kat
-        //        };
-        //        array elamanları toplamını print eden code create ediniz.
+        String str = "AAAABBBCCCDEF";
 
-        int arr[][] = {
-                {3, 5},//0. kat
-                {12, 13, 15},//1. kat
-                {103, 107, 111, 121},//2. kat
-        };
-        int toplam = 0;//array elamlarının toplanacagı boş kutu
-
-        for (int kat = 0; kat <= 2; kat++) {//kat tekrarı->outer loop:outer array
-            for (int daire = 0; daire < arr[kat].length; daire++) {// daire tekrarı->inner loop:inner array
-                toplam += arr[kat][daire];
-            }
-        }
-        System.out.println("array elamanları toplamı = " + toplam);
 
     }
+
 }
 
 
